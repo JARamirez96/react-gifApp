20f0cd53b426a870ffe540e3a4df8b59263e5ef5
Josue Ramirez <josuealexis96@gmail.com>
2020-09-21 17:11:02 -0500

    docs agregado

---
+++ docs/precache-manifest.984e98ddf8e940a45513c86b93e4e143.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72626a5c5150c225aa80b200d381590b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "617cf411507631f7d951",
+    "url": "/static/css/main.74fc712c.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "617cf411507631f7d951",
+    "url": "/static/js/main.d67b6291.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
